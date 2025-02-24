f9587c9ce4bc0b0a12a87067319c62a4c0cb80db
Drew <50529377+Druthulu@users.noreply.github.com>
2025-02-24 14:03:29 -0700

    v1
    all features work
    tests success
    SP/MP

---
+++ GalaxyFarFarAway.cs
@@ -0,0 +1,259 @@
+﻿using System.Reflection;
+using System.Xml;
+using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+
+public class GalaxyFarFarAway : IModApi
+{
+    public static string modsFolderPath;
+    public static string cubeMap = "ManyGalaxies";
+    public static string basePath = "#@modfolder(GalaxyFarFarAway):Resources/";
+    public static string unityPath = "GalaxiesRealistic.unity3d?";
+
+    //public static string testPath = "#@modfolder(GalaxyFarFarAway):Resources/GalaxyColorful01.unity3d?GalaxyColorful01";
+
+    public static bool shuffleGalaxies = true;
+    private static bool isInstantiated = false;
+    GameObject skyManagerObject;
+    public static string galaxyType = "all";
+    public List<string> galaxyTypeResults = new List<string>
+    {
+        "all", "realistic", "colorful", "abstract", "galaxy", "nebula", "spiral", "galacticString"
+    };
+    public static List<string> galaxyNameResults = new List<string>
+    {
+        "BlueGreenNebula", "BlueNebula", "GreenNebula", "ManyGalaxies", "MilkyWayGalaxy",
+        "PinkGalaxyCollision", "PinkNebula", "PurpleGalaxy", "RedOrangeNebula",
+        "bubble1", "bubble2", "chaos1", "chaos2", "chaos3", "square1",
+        "galaxy1", "galaxy2", "galaxy3", "galaxy4", "galaxy5", "galaxy6",
+        "nebula1", "nebula2", "nebula3",
+        "spiral1", "spiral2", "spiral3", "spiral4", "spiral5",
+        "string1", "string2", "string3", "string4"
+    };
+    public static int shuffleInterval = 1;
+
+    public void InitMod(Mod _modInstance)
+    {
+        Log.Out(" Loading Patch: " + base.GetType().ToString());
+        modsFolderPath = _modInstance.Path;
+        ReadXML();
+        unityPath = PathBuild(cubeMap);
+        //ModEvents.GameStartDone.RegisterHandler(GameStart);
+        if (!isInstantiated)
+        {
+            Log.Out("[GalaxyFarFarAway] Instantiating MonoBehaviour");
+            skyManagerObject = new GameObject("GFFAGalaxyManager");
+            skyManagerObject.AddComponent<MonoGFFA>();
+            isInstantiated = true;
+        }
+
+        ModEvents.WorldShuttingDown.RegisterHandler(WorldShuttingDown);
+        Harmony harmony = new Harmony(base.GetType().ToString());
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+    /*
+    void GameStart()
+    {
+        if (shuffleGalaxies)
+        {
+            if (!isInstantiated)
+            {
+                Log.Out("[GalaxyFarFarAway] Instantiating MonoBehaviour");
+                skyManagerObject = new GameObject("GFFAGalaxyManager");
+                skyManagerObject.AddComponent<MonoGFFA>();
+                isInstantiated = true;
+            }
+        }
+    }
+    */
+
+    void WorldShuttingDown()
+    {
+        if (shuffleGalaxies)
+        {
+            if (isInstantiated)
+            {
+                Log.Out("[GalaxyFarFarAway] Deleting MonoBehaviour");
+                UnityEngine.Object.Destroy(skyManagerObject);
+                isInstantiated = false;
+            }
+        }
+    }
+    public static string PathBuild(string cubeMapName)
+    {
+        var ranges = new (int start, int end, string path)[]
+        {
+            (0, 8, "GalaxiesRealistic.unity3d?"),
+            (9, 14, "GalaxyAbstract.unity3d?"),
+            (15, 20, "GalaxyColorfulGalaxies.unity3d?"),
+            (21, 23, "GalaxyNebula.unity3d?"),
+            (24, 28, "GalaxySpiral.unity3d?"),
+            (29, 32, "GalaxyString.unity3d?")
+        };
+
+        int i = galaxyNameResults.IndexOf(cubeMapName);
+        foreach (var range in ranges)
+        {
+            if (i >= range.start && i <= range.end)
+            {
+                unityPath = range.path;
+                return unityPath;
+            }
+        }
+        return unityPath; // should never return here
+    }
+
+    void ReadXML()
+    {
+        using (XmlReader xmlReader = XmlReader.Create(modsFolderPath + "\\settings.xml"))
+        {
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element)
+                {
+                    if (xmlReader.Name.ToString() == "shuffleGalaxies")
+                    {
+                        string temp = xmlReader.ReadElementContentAsString();
+                        if (!bool.TryParse(temp, out shuffleGalaxies))
+                        {
+                            Log.Warning($"[GalaxyFarFarAway] Failed to read settings.xml setting shuffleGalaxies. Using default of {shuffleGalaxies}");
+                            Log.Warning($"[GalaxyFarFarAway] Failed settings.xml read should be reviewed and mod reinstalled if needed");
+                        }
+                    }
+                    if (shuffleGalaxies)
+                    {
+                        if (xmlReader.Name.ToString() == "galaxyType")
+                        {
+                            try
+                            {
+                                var result = xmlReader.ReadElementContentAsString().ToLower();
+                                if (galaxyTypeResults.Contains(result))
+                                {
+                                    galaxyType = result;
+                                }
+                                else
+                                {
+                                    Log.Warning($"[GalaxyFarFarAway] Non Valid settings.xml galaxyType. Using default of {galaxyType}");
+                                    Log.Warning($"[GalaxyFarFarAway] Failed settings.xml read should be reviewed and mod reinstalled if needed");
+                                }
+                                
+                            }
+                            catch
+                            {
+                                Log.Warning($"[GalaxyFarFarAway] Failed to read settings.xml galaxyType. Using default of {galaxyType}");
+                                Log.Warning($"[GalaxyFarFarAway] Failed settings.xml read should be reviewed and mod reinstalled if needed");
+                            }
+                        }
+                        if (xmlReader.Name.ToString() == "shuffleInterval")
+                        {
+                            string temp = xmlReader.ReadElementContentAsString();
+                            if (!int.TryParse(temp, out shuffleInterval))
+                            {
+                                Log.Warning($"[GalaxyFarFarAway] Failed to read settings.xml setting shuffleInterval. Using default of {shuffleInterval}");
+                                Log.Warning($"[GalaxyFarFarAway] Failed settings.xml read should be reviewed and mod reinstalled if needed");
+                            }
+                        }
+                    }
+                    if (!shuffleGalaxies)
+                    {
+                        if (xmlReader.Name.ToString() == "galaxyName")
+                        {
+                            try
+                            {
+                                var result = xmlReader.ReadElementContentAsString().ToLower();
+                                if (galaxyNameResults.Contains(result))
+                                {
+                                    cubeMap = result;
+                                }
+                                else
+                                {
+                                    Log.Warning($"[GalaxyFarFarAway] Non Valid settings.xml galaxyName. Using default of {cubeMap}");
+                                    Log.Warning($"[GalaxyFarFarAway] Failed settings.xml read should be reviewed and mod reinstalled if needed");
+                                }
+                            }
+                            catch
+                            {
+                                Log.Warning($"[GalaxyFarFarAway] Failed to read settings.xml galaxyName. Using default of {cubeMap}");
+                                Log.Warning($"[GalaxyFarFarAway] Failed settings.xml read should be reviewed and mod reinstalled if needed");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+/* works, dont break it */
+
+[HarmonyPatch(typeof(SkyManager), "Init")]
+public class SetSkyTexture_Patch
+{
+    public static void Postfix()
+    {
+        if (!GalaxyFarFarAway.shuffleGalaxies)
+        {
+            int propColorMap = Shader.PropertyToID("_OuterSpaceCube");
+            string skySprite = "AtmosphereSphere";
+            Transform skyTransform = SkyManager.skyManager.transform.FindInChildren(skySprite);
+
+            //Cubemap skyTexture = DataLoader.LoadAsset<Cubemap>(GalaxyFarFarAway.testPath);
+            string fullPath = GalaxyFarFarAway.basePath + GalaxyFarFarAway.unityPath + GalaxyFarFarAway.cubeMap;
+            Cubemap skyTexture = DataLoader.LoadAsset<Cubemap>(fullPath);
+
+            MeshRenderer renderer;
+            if (skyTransform.TryGetComponent<MeshRenderer>(out renderer))
+            {
+                if (renderer.material != null && skyTexture != null)
+                {
+                    Log.Out($"[GalaxyFarFarAway] Replacing Skybox cubemap with {GalaxyFarFarAway.cubeMap}");
+                    renderer.material.SetTexture(propColorMap, skyTexture);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+// attempt to load the cubemap material directly, didn't work
+
+/*
+[HarmonyPatch(typeof(SkyManager), "Init")]
+public class SetSkyboxMaterial_Patch
+{
+    public static void Postfix()
+    {
+        // Load your custom skybox material (replace with your mod's material path)
+        //DiverseSpaceMaterial
+        string location = "#@modfolder(GalaxyFarFarAway):Resources/galaxies.unity3d?DiverseSpaceMaterial";
+        Material skyboxMaterial = DataLoader.LoadAsset<Material>(location);
+
+        if (skyboxMaterial != null)
+        {
+            // Apply the loaded skybox material to the scene's skybox
+            RenderSettings.skybox = skyboxMaterial;
+            Debug.Log("[MMM] Custom Skybox Material Applied!");
+        }
+        else
+        {
+            Debug.LogError("[MMM] Failed to load custom skybox material.");
+        }
+    }
+}*/
+
+
+// Notes, extra texture information
+/*
+2025-02-18T16:59:52 58.120 INF [MMM] Found MeshRenderer on: MoonSprite
+2025-02-18T16:59:52 58.123 INF [MMM] Property on MoonSprite: _ColorMap
+2025-02-18T16:59:52 58.125 INF [MMM] Found MeshRenderer on: AtmosphereSphere
+2025-02-18T16:59:52 58.128 INF [MMM] Property on AtmosphereSphere: _OuterSpaceCube
+2025-02-18T16:59:52 58.130 INF [MMM] Found MeshRenderer on: CloudsSphere
+2025-02-18T16:59:52 58.132 INF [MMM] Property on CloudsSphere: _CloudBGTex
+2025-02-18T16:59:52 58.135 INF [MMM] Property on CloudsSphere: _CloudBlendTex
+2025-02-18T16:59:52 58.137 INF [MMM] Property on CloudsSphere: _CloudMainTex
+2025-02-18T16:59:52 58.140 INF [MMM] Property on CloudsSphere: _DebugTex
+*/
---
+++ MonoGFFA.cs
@@ -0,0 +1,309 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System.Linq;
+using System.Collections;
+
+public class MonoGFFA : MonoBehaviour
+{
+    float timeSinceLastUpdate = 300f; // Timer for update intervals
+    const float updateInterval = 300f; // Update interval in seconds
+    //const float updateInterval = 3f; // Test interval
+    int lastInterval = 0;
+    //int testLogicIndex = 0;
+    //static string basePath = "#@modfolder(GalaxyFarFarAway):Resources/";
+    //ResourceRequest loadRequest;
+    //string galaxyName;
+    List<Cubemap> cubemaps = new List<Cubemap>();
+    string[] assetBundlePathsOptions = {
+        $"{GalaxyFarFarAway.modsFolderPath}/Resources/GalaxiesRealistic.unity3d",
+        $"{GalaxyFarFarAway.modsFolderPath}/Resources/GalaxyAbstract.unity3d",
+        $"{GalaxyFarFarAway.modsFolderPath}/Resources/GalaxyColorfulGalaxies.unity3d",
+        $"{GalaxyFarFarAway.modsFolderPath}/Resources/GalaxyNebula.unity3d",
+        $"{GalaxyFarFarAway.modsFolderPath}/Resources/GalaxySpiral.unity3d",
+        $"{GalaxyFarFarAway.modsFolderPath}/Resources/GalaxyString.unity3d"
+    };
+    string[] olDassetBundlePathsOptionsOld = {
+        $"#@modfolder(GalaxyFarFarAway):Resources/GalaxiesRealistic.unity3d",
+        $"#@modfolder(GalaxyFarFarAway):Resources/GalaxyAbstract.unity3d",
+        $"#@modfolder(GalaxyFarFarAway):Resources/GalaxyColorfulGalaxies.unity3d",
+        $"#@modfolder(GalaxyFarFarAway):Resources/GalaxyNebula.unity3d",
+        $"#@modfolder(GalaxyFarFarAway):Resources/GalaxySpiral.unity3d",
+        $"#@modfolder(GalaxyFarFarAway):Resources/GalaxyString.unity3d"
+    };
+
+    List<string> assetBundlePaths = new List<string>();
+    /*
+    List<String> galaxyListReal = new List<String>
+    {
+        "BlueGreenNebula", "BlueNebula", "GreenNebula", "ManyGalaxies",
+        "MilkyWayGalaxy", "PinkGalaxyCollision", "PinkNebula", "PurpleGalaxy", "RedOrangeNebula"
+    };
+    List<String> galaxyListColor = new List<String>
+    {
+        "bubble1", "bubble2", "chaos1", "chaos2", "chaos3", "square1",
+        "galaxy1", "galaxy2", "galaxy3", "galaxy4", "galaxy5", "galaxy6",
+        "nebula1", "nebula2", "nebula3",
+        "spiral1", "spiral2", "spiral3", "spiral4", "spiral5",
+        "string1", "string2", "string3", "string4"
+    };
+    List<String> galaxyListAbstract = new List<String>
+    {
+        "bubble1", "bubble2", "chaos1", "chaos2", "chaos3", "square1"
+    };
+    List<String> galaxyListGalaxy = new List<String>
+    {
+        "galaxy1", "galaxy2", "galaxy3", "galaxy4", "galaxy5", "galaxy6"
+    };
+    List<String> galaxyListNebula = new List<String>
+    {
+        "nebula1", "nebula2", "nebula3"
+    };
+    List<String> galaxyListSpiral = new List<String>
+    {
+        "spiral1", "spiral2", "spiral3", "spiral4", "spiral5"
+    };
+    List<String> galaxyListString = new List<String>
+    {
+        "string1", "string2", "string3", "string4"
+    };
+    */
+    string galaxyType = "all";
+    List<String> galaxyListTotal = new List<String>();
+    int playerId = -1;
+    int cubemapCount = 0;
+    int cubemapRunningCount = 0;
+    bool cubemapsLoaded = false;
+
+    void Start()
+    {
+        galaxyType = GalaxyFarFarAway.galaxyType;
+
+        if (galaxyType == "all")
+        {
+            //galaxyListTotal = galaxyListReal.Concat(galaxyListColor).ToList();
+            assetBundlePaths = assetBundlePathsOptions.ToList();
+            cubemapCount = 33;
+        }
+        else if (galaxyType == "realistic")
+        {
+            //galaxyListTotal = galaxyListReal;
+            assetBundlePaths.Add(assetBundlePathsOptions[0].Replace("/", "\\"));
+            cubemapCount = 9;
+        }
+        else if (galaxyType == "colorful")
+        {
+            //galaxyListTotal = galaxyListColor;
+            for (int i = 1; i < assetBundlePathsOptions.Length; i++)
+            {
+                assetBundlePaths.Add(assetBundlePathsOptions[i].Replace("/", "\\"));
+            }
+            cubemapCount = 24;
+        }
+        else if (galaxyType == "abstract")
+        {
+            //galaxyListTotal = galaxyListAbstract;
+            assetBundlePaths.Add(assetBundlePathsOptions[1].Replace("/", "\\"));
+            cubemapCount = 6;
+        }
+        else if (galaxyType == "galaxy")
+        {
+            //galaxyListTotal = galaxyListGalaxy;
+            assetBundlePaths.Add(assetBundlePathsOptions[2].Replace("/", "\\"));
+            cubemapCount = 6;
+        }
+        else if (galaxyType == "nebula")
+        {
+            //galaxyListTotal = galaxyListNebula;
+            assetBundlePaths.Add(assetBundlePathsOptions[3].Replace("/", "\\"));
+            cubemapCount = 3;
+        }
+        else if (galaxyType == "spiral")
+        {
+            //galaxyListTotal = galaxyListSpiral;
+            assetBundlePaths.Add(assetBundlePathsOptions[4].Replace("/", "\\"));
+            cubemapCount = 5;
+        }
+        else if (galaxyType == "galacticString")
+        {
+            //galaxyListTotal = galaxyListString;
+            assetBundlePaths.Add(assetBundlePathsOptions[5].Replace("/", "\\"));
+            cubemapCount = 4;
+        }
+
+        // Start the coroutine to load the assets
+        StartCoroutine(LoadCubemapsFromBundlesAsync());
+        
+        /*
+        while (playerId < 0)
+        {
+            spawnCheck();
+        }*/
+    }
+
+    void Update()
+    {
+        // prevent logic until player entity exists
+        if (playerId > 0)
+        {
+            timeSinceLastUpdate += Time.deltaTime;
+            if (timeSinceLastUpdate >= updateInterval)
+            {
+                // prevent update until all assets loaded
+                if (cubemapsLoaded)
+                {
+                    timeSinceLastUpdate = 0f;
+                    int intervalNumber = intervalCheck();
+
+                    if (intervalNumber > lastInterval)
+                    {
+                        lastInterval = intervalNumber;
+                        SetSkybox(intervalNumber);
+                    }
+                }
+                else
+                {
+                    timeSinceLastUpdate = 295f; // 5sec instead of 5min for quicker intial checks.
+                    Log.Out($"[GalaxyFarFarAway] Still loading assets {cubemapRunningCount} out of {cubemapCount}");
+                }
+            }
+        }
+        else
+        {
+            spawnCheck();
+        }
+        
+    }
+
+    void spawnCheck()
+    {
+        // This will prevent a few safe red errors while loading into a multiplayer server
+        var myPlayerId = GameManager.Instance.myPlayerId;
+        //Log.Out($"my playerId is: {myPlayerId}");
+        if (myPlayerId > 0)
+        {
+            playerId = myPlayerId;
+        }
+    }
+
+    int intervalCheck()
+    {
+        var shuffleInterval = (ulong)GalaxyFarFarAway.shuffleInterval * 24000UL;
+        ulong worldTime = GameManager.Instance.World.worldTime;
+        // 12pm, so no sky in view during change
+        ulong adjustedWorldTime = worldTime >= 12000UL ? worldTime - 12000UL : worldTime + 12000UL;
+        var intervalNumber = (int)(adjustedWorldTime / shuffleInterval + 1UL);
+        return intervalNumber;
+    }
+    /*
+    void UpdateSkyBox(int dayNum)
+    {
+        // Start the coroutine to load the skybox texture
+        //StartCoroutine(SetSkybox(dayNum));
+    }
+
+    void LoadCubemaps()
+    {
+        // Start the coroutine to load the assets
+        StartCoroutine(LoadCubemapsFromBundlesAsync());
+    }
+    */
+    IEnumerator LoadCubemapsFromBundlesAsync()
+    {
+        foreach (var bundlePath in assetBundlePaths)
+        {
+            
+            //Log.Out($"[GalaxyFarFarAway] attempting to load AssetBundle from {bundlePath}");
+            AssetBundleCreateRequest bundleRequest = AssetBundle.LoadFromFileAsync(bundlePath);
+            yield return bundleRequest;
+            AssetBundle assetBundle = bundleRequest.assetBundle;
+            if (assetBundle == null)
+            {
+                Log.Warning($"[GalaxyFarFarAway] Failed to load AssetBundle from {bundlePath}");
+                continue;
+            }
+            //Log.Out($"[GalaxyFarFarAway] loaded AssetBundle from {bundlePath}");
+            //Log.Out($"[GalaxyFarFarAway] attempting to load cubemaps from {bundlePath}");
+            AssetBundleRequest assetRequest = assetBundle.LoadAllAssetsAsync<Cubemap>();
+            yield return assetRequest;
+
+            // Iterate through the assets one at a time
+            foreach (var asset in assetRequest.allAssets)
+            {
+                // Check if the asset is a Cubemap
+                if (asset is Cubemap cubemap)
+                {
+                    // Add the cubemap to the list
+                    cubemaps.Add(cubemap);
+                    cubemapRunningCount++;
+                    //Log.Out($"[GalaxyFarFarAway] Loaded cubemap: {cubemap.name}");
+
+                    // Yield to prevent freezing, allowing the game to process other tasks
+                    yield return null;
+                }
+                yield return null;
+            }
+            yield return null;
+
+            /*
+            //Cubemap[] loadedCubemaps = assetRequest.allAssets as Cubemap[];
+            Cubemap[] loadedCubemaps = assetRequest.allAssets.OfType<Cubemap>().ToArray();
+            if (loadedCubemaps != null)
+            {
+                foreach (var cubemap in loadedCubemaps)
+                {
+                    cubemaps.Add(cubemap);
+                    cubemapRunningCount++;
+                    Log.Out($"[GalaxyFarFarAway] Loaded Cubemap: {cubemap.name}");
+                }
+            }
+            */
+            assetBundle.Unload(false);
+        }
+        cubemapsLoaded = true;
+        Log.Out($"[GalaxyFarFarAway] All cubemaps loaded from Asset Bundles! count: {cubemaps.Count}");
+    }
+
+    public void SetSkybox(int dayNum)
+    {
+        // Main logic to choose a galaxy (for example)
+        System.Random rand = new System.Random(dayNum);
+        int cubemapIndex = rand.Next(cubemaps.Count);
+
+        /*
+        // Optionally, test logic
+        cubemapIndex = testLogicIndex++;
+        //timeSinceLastUpdate = 55f;
+        lastDay = 0;
+        if (testLogicIndex > cubemaps.Count - 1)
+        {
+            testLogicIndex = 0;
+        }
+        */
+
+        if (cubemaps.Count > cubemapIndex)
+        {
+            int propColorMap = Shader.PropertyToID("_OuterSpaceCube");
+            string skySprite = "AtmosphereSphere";
+            Transform skyTransform = SkyManager.skyManager.transform.FindInChildren(skySprite);
+
+            Cubemap cubemapToApply = cubemaps[cubemapIndex];
+            if (cubemapToApply != null)
+            {
+                MeshRenderer renderer;
+                if (skyTransform.TryGetComponent<MeshRenderer>(out renderer))
+                {
+                    if (renderer.material != null)
+                    {
+                        renderer.material.SetTexture(propColorMap, cubemapToApply);
+                        Log.Out($"[GalaxyFarFarAway] Applied cubemap: {cubemapToApply.name}");
+                    }
+                }
+            }
+        }
+        else
+        {
+            //Log.Warning("[GalaxyFarFarAway] Invalid cubemap index.");
+        }
+    }
+}
